58fb5f4c4016c3e36a6a18b396863ab65edca8c6
omar-bay <omarmb2002@gmail.com>
2022-06-08 12:07:31 +0300

    creating invoice but name is Draft

--- models/estate_property.py
+++ models/estate_property.py
@@ -4,10 +4,27 @@ class EstateProperty(models.Model):
     _inherit = "estate.property"
 
     def action_do_sold(self):
-        # omar
-        import logging
-        _logger = logging.getLogger(__name__)
-        output = "\n\n\n\n"+str("Yaay, the inherited thing is working on sold")+"\n\n\n\n"
-        _logger.exception('In the Log file to view the  %s', output)
-        # omar
-        return super().action_do_sold()
+        res = super().action_do_sold()
+        journal = self.env["account.journal"].search([("type", "=", "sale")], limit=1)
+        for prop in self:
+            self.env["account.move"].create(
+                {
+                    "partner_id": prop.buyer.id,
+                    "move_type": "out_invoice",
+                    "journal_id": journal.id,
+                    "invoice_line_ids": [
+                        (0, 0, {
+                                "name": prop.name,
+                                "quantity": 1.0,
+                                "price_unit": prop.selling_price * 6.0 / 100.0,
+                        }, ), (0,0,
+                            {
+                                "name": "Administrative fees",
+                                "quantity": 1.0,
+                                "price_unit": 100.0,
+                            },
+                        ),
+                    ],
+                }
+            )
+        return res
